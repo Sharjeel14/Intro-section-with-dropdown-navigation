cc0b895138d35a024707ee97514a168caeb47326
Sharjeel14 <sharjeelr117@gmail.com>
2022-08-09 23:35:52 +0500

    intro-section-with-dropdown

---
+++ script.js
@@ -0,0 +1,35 @@
+/* When the user clicks on the button, 
+toggle between hiding and showing the dropdown content */
+jQuery(document).ready(function (e) {
+    function t(t) {
+        e(t).bind("click", function (t) {
+            t.preventDefault();
+            e(this).parent().fadeOut()
+        })
+    }
+    e(".dropdown-toggle").click(function () {
+        var t = e(this).parents(".button-dropdown").children(".dropdown-menu").is(":hidden");
+        e(".button-dropdown .dropdown-menu").hide();
+        e(".button-dropdown .dropdown-toggle").removeClass("active");
+        if (t) {
+            e(this).parents(".button-dropdown").children(".dropdown-menu").toggle().parents(".button-dropdown").children(".dropdown-toggle").addClass("active")
+        }
+    });
+    e(document).bind("click", function (t) {
+        var n = e(t.target);
+        if (!n.parents().hasClass("button-dropdown")) e(".button-dropdown .dropdown-menu").hide();
+    });
+    e(document).bind("click", function (t) {
+        var n = e(t.target);
+        if (!n.parents().hasClass("button-dropdown")) e(".button-dropdown .dropdown-toggle").removeClass("active");
+    })
+});
+
+
+// Navbar Toggle For Mobile
+const menuToggle = document.querySelector('.toggle');
+const navigation = document.querySelector('.navigation');
+menuToggle.onclick = function () {
+    menuToggle.classList.toggle('active');
+    navigation.classList.toggle('active');
+}
